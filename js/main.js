24ca67ea5884c60460886be5b21a0076ae70b057
Javier Valdez <jv.javierval@gmail.com>
2022-05-24 21:47:47 -0300

    Crea funcion para cargar paises

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("Arrancando!");
+const getCountries = async () => {
+    const result = await fetch("https://restcountries.com/v3.1/all");
+    const results = await result.json();
+    results.forEach(element => {
+        createCard(element)
+        
+    });
+}
+
+getCountries()
+
